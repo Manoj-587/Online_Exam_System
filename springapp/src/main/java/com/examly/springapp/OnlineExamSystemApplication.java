5416fd871580096a4a1e28d6768fd0b00a29717a
NAGUL13 <nagulpranav13@gmail.com>
2025-09-02 10:00:54 +0530

    db connected

--- springapp/src/main/java/com/examly/springapp/OnlineExamSystemApplication.java
+++ springapp/src/main/java/com/examly/springapp/OnlineExamSystemApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OnlineExamSystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(OnlineExamSystemApplication.class, args);
+        System.out.print("Hiii");
     }
 }
